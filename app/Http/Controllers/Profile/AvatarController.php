4523904f7eddaa4e49aa30ac036b7db0a9989acc
Arthur Faria <arthur.agf@gmail.com>
2024-02-24 21:00:01 +0000

    Removes the old file when user edits his avatar

--- app/Http/Controllers/Profile/AvatarController.php
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateAvatarRequest;
+use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function update(UpdateAvatarRequest $request) {
+        $oldAvatar = $request->user()->avatar;
+
+        $path = Storage::disk('public')->put('avatars', $request->file('avatar'));
+
         auth()->user()->update([
-            'avatar' => $request->file('avatar')->store('avatars', 'public')
+            'avatar' => $path
         ]);
+
+        if ($oldAvatar)
+            Storage::disk('public')->delete($oldAvatar);
         return back()->with('message', 'Avatar is changed');
     }
 }
